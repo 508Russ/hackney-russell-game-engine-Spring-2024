13d8393243dbefb8ed104f9628ddc5e0188bf7c0
508Russ <160270345+508Russ@users.noreply.github.com>
2024-03-12 14:16:08 -0700

    changed background color added more coins

--- sprites.py
+++ sprites.py
@@ -97,7 +97,7 @@ class Wall(pg.sprite.Sprite):
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
         self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(BLUE)
+        self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
